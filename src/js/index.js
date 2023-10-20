6cbcc65d519e5282c4082802f3ecce77b21b7f49
Pedro Aquino <pedroh_aquino@hotmail.com>
2023-10-20 19:46:32 +0100

    Atualizando

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-const formulario = document.querySelector(".formulario")
+const formulario = document.querySelector("#formulario")
 const campo = document.querySelectorAll(".campo")
 const input = document.querySelectorAll(".input")
 
@@ -7,16 +7,34 @@ formulario.addEventListener('submit', function (event) {
     event.preventDefault();
     verificarCampos()
 
+    if (verificarSeEnviaParaServidor()) {
+        formulario.submit()
+        alert("Mensagem enviada")
+    }
+
+
 })
 
-
 function verificarCampos() {
+    let valor = 0
+    input.forEach(function (elemento) {
+        if (elemento.value.length < 1) {
+            campo[valor].classList.add("campo-nao-preenchido");
+        } else {
+            campo[valor].classList.remove("campo-nao-preenchido");
+            campo[valor].classList.add("campo-preenchido")
+        }
+        valor++
+    }
+    )
+}
+
+function verificarSeEnviaParaServidor() {
     for (let i = 0; i < input.length; i++) {
         if (input[i].value.length < 1) {
-            campo[i].classList.add("campo-nao-preenchido");
-        } else {
-            campo[i].classList.remove("campo-nao-preenchido");
-            campo[i].classList.add("campo-preenchido")
+            return false
+
         }
     }
+    return true
 }
